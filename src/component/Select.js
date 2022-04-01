246ce39b477f7af581f75da5ab57844085748ce8
Lucas5k <lucascabral112347@gmail.com>
2022-04-01 01:10:53 -0300

    request 5

---
+++ src/component/Select.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Select extends Component {
+  render() {
+    const {
+      label,
+      name,
+      onChange,
+      value,
+      id,
+      options,
+    } = this.props;
+    return (
+      <label htmlFor={ name }>
+        { label }
+        <select
+          name={ name }
+          id={ id }
+          required
+          onChange={ onChange }
+          value={ value }
+        >
+          {
+            options.map((option, index) => (
+              <option key={ index }>{ option }</option>
+            ))
+          }
+        </select>
+      </label>
+    );
+  }
+}
+
+Select.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.string,
+  ).isRequired,
+};
+
+export default Select;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,17 +3,106 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { fetchApiCurrencies } from '../actions';
 import Header from '../component/Header';
+import Select from '../component/Select';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      valor: '',
+      descrição: '',
+      pagamento: '',
+      categoria: '',
+      moeda: '',
+    };
+  }
+
   componentDidMount() {
     const { fetchResultsApi } = this.props;
     fetchResultsApi();
   }
 
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { valor, descrição, pagamento, categoria, moeda } = this.state;
+    const { currencies } = this.props;
     return (
       <section>
         <Header />
+        <label htmlFor="valorInput">
+          Valor:
+          <input
+            data-testid="value-input"
+            id="valorInput"
+            name="valor"
+            type="number"
+            value={ valor }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <Select
+          onChange={ this.handleChange }
+          value={ moeda }
+          label="Moeda: "
+          id="moedaInput"
+          name="moedaInput"
+          options={ currencies }
+        />
+        <label
+          data-testid="method-input"
+          htmlFor="paymentInput"
+        >
+          Método Pagamento:
+          <select
+            id="paymentInput"
+          >
+            <option
+              value={ pagamento }
+              onChange={ this.handleChange }
+            >
+              Dinheiro
+            </option>
+            <option value={ pagamento }>Cartão de crédito</option>
+            <option value={ pagamento }>Cartão de débito</option>
+          </select>
+        </label>
+        <label
+          data-testid="tag-input"
+          htmlFor="dropdownInput"
+        >
+          Categoria:
+          <select
+            id="dropdownInput"
+          >
+            <option
+              value={ categoria }
+              onChange={ this.handleChange }
+            >
+              Alimentação
+            </option>
+            <option value={ categoria }>Lazer</option>
+            <option value={ categoria }>Trabalho</option>
+            <option value={ categoria }>Transporte</option>
+            <option value={ categoria }>Saúde</option>
+          </select>
+        </label>
+        <label htmlFor="descriptionInput">
+          Descrição:
+          <input
+            data-testid="description-input"
+            id="descriptionInput"
+            name="descrição"
+            value={ descrição }
+            onChange={ this.handleChange }
+          />
+        </label>
       </section>
     );
   }
@@ -23,8 +112,13 @@ const mapDispatchToProps = (dispatch) => ({
   fetchResultsApi: () => dispatch(fetchApiCurrencies()),
 });
 
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
 Wallet.propTypes = {
   fetchResultsApi: PropTypes.func.isRequired,
+  currencies: PropTypes.func.isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(Wallet);
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
